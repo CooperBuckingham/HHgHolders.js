222ebbda1112da65e1804553527fd863bb57c2b5
cooper buckingham <cooper.buckingham@gmail.com>
2015-02-06 13:42:49 -0800

    started basic vector2 class

---
+++ S2DAction.js
@@ -0,0 +1,13 @@
+function S2DAction(owner, subType, args){
+	this.owner = owner;
+	this.subType = subType;
+	this.args = args;
+	this.startTime = +new Date;
+	this.whatShouldIDoThisFrame = function(deltaT, now){
+		switch(this.subType){
+			case "move":
+			
+
+		}
+	} ;
+}
