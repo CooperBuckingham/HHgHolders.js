a37614e1962f142a4cd41f0887e280e3d833ff24
cooper buckingham <cooper.buckingham@gmail.com>
2015-02-08 12:54:39 -0800

    partway through reformatting the base classes

--- HHgScene.js
+++ HHgScene.js
@@ -11,24 +11,21 @@ function startHHgScene(){
 	console.log("scene setup start");
 	HHgScene = new HHgHolder(400,300, "stop");
 
-	HHgScene.halfWidth = this.myHeight / 2.0;
-	HHgScene.halfHeight = this.myWidth / 2.0;
-
-	HHgScene.addThisHolder =function(holder){
+	HHgScene.addThisHolder = function(holder){
 		
 		var div =document.createElement("div");
-		holder.myDiv = div;
+		holder.setDiv(div);
 		div.style.display ="inline-block";
 		div.style.position = "absolute";
 		
 		div.style.backgroundColor ="red";
 		div.style.border = "2px solid black";
-		div.style.width = "" + holder.myWidth + "px";
-		div.style.height ="" + holder.myHeight + "px";
+		div.style.width = "" + holder.getHeight() + "px";
+		div.style.height ="" + holder.getWidth() + "px";
 		var centricConversion = convertTo00Centric(holder.getPositionInScreen());
-		div.style.left ="" + centricConversion.myX +"px";
-		div.style.bottom ="" + (centricConversion.myY) + "px";
-		div.myHolder = holder;
+		div.style.left ="" + centricConversion.getX() +"px";
+		div.style.bottom ="" + (centricConversion.getY()) + "px";
+		div.setHolder(holder);
 		
 		theAll.appendChild(div);
 	};
