ea59c2b0a42486661b3d2967670257ba801084ef
cooper buckingham <cooper.buckingham@gmail.com>
2015-02-08 20:30:26 -0800

    error free - div placement working centric

--- HHgScene.js
+++ HHgScene.js
@@ -1,44 +1,84 @@
 
-
-function convertTo00Centric(xyPos){
-	return new HHgVector2(xyPos.myX + this.halfWidth, xyPos.myY - this.halfHeight);
+var HHgSceneInternal = function(){
+	
 }
+
+
 
 var HHgScene;
 var theAll = document.getElementById("all");
 
-function startHHgScene(){
+function doStartHHgScene(){
 	console.log("scene setup start");
-	HHgScene = new HHgHolder(400,300);
+	HHgScene = new HHgHolder(960,540);
 
-	HHgScene.addThisHolder = function(holder){
+	//we add all the custom functions to the scene here
+	doAddFunctionsToScene(HHgScene);
+
+	HHgScene.doMoveToNewParent("stop");
+	var sceneDiv = HHgScene.getDiv();
+	console.log(sceneDiv.style.left);
+	console.log(HHgScene.getHalfWidth());
+	sceneDiv.style.left = parseInt(sceneDiv.style.left) - HHgScene.getHalfWidth();
+	sceneDiv.style.bottom = parseInt(sceneDiv.style.bottom) - HHgScene.getHalfHeight();
+	sceneDiv.style.backgroundColor = "blue";
+	console.log("added scene");
+
+	var testBlock = new HHgHolder(50,50);
+	testBlock.doMoveToNewParent(HHgScene, new HHgVector2(0,0));
+
+	var testBlock = new HHgHolder(50,50);
+	testBlock.doMoveToNewParent(HHgScene, new HHgVector2(-100,100));
+
+	var testBlock = new HHgHolder(50,50);
+	testBlock.doMoveToNewParent(HHgScene, new HHgVector2(100,100));
+
+	var testBlock = new HHgHolder(50,50);
+	testBlock.doMoveToNewParent(HHgScene, new HHgVector2(100,-100));
+
+
+	var testBlock = new HHgHolder(50,50);
+	testBlock.doMoveToNewParent(HHgScene, new HHgVector2(-100,-100));
+
+
+
+}
+
+function doAddFunctionsToScene(scene){
+		scene.doAddThisHolder = function(holder){
 		
-		var div =document.createElement("div");
+		var div = document.createElement("div");
 		holder.setDiv(div);
 		div.style.display ="inline-block";
 		div.style.position = "absolute";
 		div.style.backgroundColor ="red";
 		div.style.border = "2px solid black";
-		div.style.width = "" + holder.getHeight() + "px";
-		div.style.height ="" + holder.getWidth() + "px";
-		var centricConversion = convertTo00Centric(holder.getPositionInScreen());
+		div.style.width = "" + holder.getWidth() + "px";
+		div.style.height ="" + holder.getHeight() + "px";
+		var centricConversion = this.doConvertTo00Centric( holder.getPositionInScreen() );
 		div.style.left ="" + centricConversion.getX() +"px";
-		div.style.bottom ="" + (centricConversion.getY()) + "px";
+		div.style.bottom ="" + centricConversion.getY() + "px";
 		div.id = holder.getHash();
 		
 		theAll.appendChild(div);
 	};
 
+	scene.doUpdateThisHolder = function(holder){
+		var div = holder.getDiv();
+		div.style.width = "" + holder.getWidth() + "px";
+		div.style.height ="" + holder.getHeight() + "px";
+		var centricConversion = this.doConvertTo00Centric(holder.getPositionInScreen());
+		div.style.left ="" + centricConversion.getX() +"px";
+		div.style.bottom ="" + centricConversion.getY() + "px";
+	}
 
-HHgScene.doMoveToNewParent("stop");
-console.log("added scene");
-
-	var testBlock = new HHgHolder(50,50);
-	
-
-testBlock.doMoveToNewParent();
-console.log("added test");
+	scene.doConvertTo00Centric = function(xyPos){
+		//return xyPos;
+		return new HHgVector2(xyPos.getX() + this.getHalfWidth(), xyPos.getY() + this.getHalfHeight());
+	}
 }
+
+
 
 
 
