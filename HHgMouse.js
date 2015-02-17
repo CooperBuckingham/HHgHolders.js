f26131a71508d2c26c2c97afc964c8d145f4696d
cooper buckingham <cooper.buckingham@gmail.com>
2015-02-17 14:26:40 -0800

    fixed z order picking on stacked divs

--- HHgMouse.js
+++ HHgMouse.js
@@ -70,13 +70,10 @@ var HHgMouse = function HHgMouse(){
 			return;
 		}
 
-return;
-		holders = findClickedHolder(holders, x, y);
-		if(holders){
-			that.doSetVars(holders, x, y);
+		
+		
+			that.doSetVars(holders[0], x, y);
 			that.clickedDownOn.doMouseDown();
-			return;
-		}
 
 		
 			
@@ -86,7 +83,7 @@ return;
 		console.log("mouseup");
 		that.mouseCircle.doHide();
 
-		return;
+		
 
 		if(that.clickedDownOn === undefined){
 			return;
@@ -107,43 +104,7 @@ return;
 		
 	}
 
-	this.findClickedHolder = function(holders, x, y){
-		var holder, canvas, xy, convertedXY = new HHgVector2(0,0),canvasSize = new HHgVector2(0,0), canvasStyleSize = new HHgVector2(0,0);
-		for(var i = 0; i < holders.length; i++){
 
-			holder = holders[i];
-
-			canvas = holder.getCanvas();
-			canvasSize.setXY(canvas.width, canvas.height);
-			canvasStyleSize.setXY(canvas.clientWidth, canvas.clientHeight);
-			convertedXY = getXYPosInDivFromMouse(holder,canvasSize,canvasStyleSize,x,y,convertedXY);
-			if( isAlphaPixel(canvas,convertedXY.getX(), convertedXY.getY() ) === false){
-				
-				return holder;
-			}
-
-		}
-		
-		return undefined;
-		
-	}
-
-	this.rotateMouse = function(mouseXY, holder){
-
-
-				var _positionInScreenNet = mouseXY;
-
-			
-				_positionInScreenNet = _positionInScreenNet.returnVectorPlusVector(HHgScene.returnHalfSizeVector());
-				
-				_positionInScreenNet = holder.returnHalfSizeVector().returnVectorSubtractedFromVector(_positionInScreenNet);
-
-				var _positionInParentOriginal = holder.getParent().getPositionInScreenOriginal().returnVectorSubtractedFromVector(mouseXY);
-				_positionInParentOriginal = _positionInParentOriginal.returnVectorScaledByInverse(holder.getParent().getScaleNet());
-				_positionInParentOriginal = _positionInParentOriginal.returnVectorRotatedAroundVectorAtAngle( holder.getParent().getPositionInScreenNet(), -1 *  holder.getParent().getRotationNet() );
-			
-				return _positionInParentOriginal;
-	}
 
 	return this; //for singleton
 
@@ -151,71 +112,9 @@ return;
 
 
 
-function getXYPosInDivFromMouse(holder,canvasSize, canvasStyleSize,xy,y,convertedXY){
-	/*
-	convertedXY.setXY(xy,y);
-	convertedXY = convertedXY.returnVectorPlusVector( holder.returnHalfSizeVector() );
-	
-	convertedXY = holder.getPositionInScreenOriginal().returnVectorSubtractedFromVector(convertedXY);
-	convertedXY.setY(canvasStyleSize.getY() - convertedXY.getY());
-	
-	convertedXY = convertedXY.returnVectorScaledBy(canvasSize.returnVectorScaledByInverse(canvasStyleSize));
-	return convertedXY;
-	*/
-//above worked for non rotated divs
-//getBoundingClientRect();
-
-var imgData = holder.getCanvas().getContext("2d").getImageData(0,0,holder.getCanvas().width, holder.getCanvas().height);
-for(var i = 0; i < imgData.data.length; i+=4){
-	if(i < imgData.data.length / 4){
-
-	}
-	imgData.data[i] = 255;
-	imgData.data[i+1] = 0;
-	imgData.data[i+2] = 0;
-	imgData.data[i+3] = 255;
-
-	if(i < imgData.data.length / 4){
-		
-	imgData.data[i+1] = 255;
-	}
-}
-
-holder.getCanvas().getContext("2d").putImageData(imgData,0,0);
-
-return;
-
-
-	convertedXY.setXY(xy,y);
-	console.log("mouse P:" + convertedXY.returnPretty());
-	console.log("canvas P: " + holder.getCanvas().getBoundingClientRect().left + " " + holder.getCanvas().getBoundingClientRect().bottom);
-	convertedXY = convertedXY.returnVectorPlusVector( holder.returnHalfSizeVector() );
-	//convertedXY = rotateMouse(convertedXY, holder);
-	
-	convertedXY = holder.getPositionInScreenOriginal().returnVectorSubtractedFromVector(convertedXY);
-	convertedXY.setY(canvasStyleSize.getY() - convertedXY.getY());
-	
-	convertedXY = convertedXY.returnVectorScaledBy(canvasSize.returnVectorScaledByInverse(canvasStyleSize));
-	
-	return convertedXY;
-	
-	
-}
-
 //alpha test for mouse click
-function isAlphaPixel(canvas, xy, y){
-	return canvas.getContext('2d').getImageData(xy, y, 1, 1).data[3] > .15 ? false : true;
-}
 
 
-function getpixelcolour(canvas, x, y) {
-  var cx = canvas.getContext('2d');
-  var pixel = cx.getImageData(x, y, 1, 1);
-  return {
-    r: pixel.data[0],
-    g: pixel.data[1],
-    b: pixel.data[2],
-    a: pixel.data[3]
-  };
-}
+
+
 
