00b437f40487c40b05a1631b760c2964cff84577
cooper buckingham <cooper.buckingham@gmail.com>
2015-03-18 22:44:29 -0700

    action and props work

--- HHgAction.js
+++ HHgAction.js
@@ -25,29 +25,26 @@ var HHgAction = function (owner, totalDelta, startValue, totalTime, ease, onComp
 	this.lastPercent = 0;
 	this.deltaPercent = 0;
 
-	
 	this.deltaValue = this.isXY ? new HHgVector2(0,0) : 0;
 	this.lastValue = this.isXY ? new HHgVector2(0,0) : 0;
-	
 
 	if(ease){
-		
+
 		if(ease.easeIn > 1 ) ease.easeIn /= 100;
 		if(ease.easeOut > 1) ease.easeOut /= 100;
-		this.easeInPercent = ease.easeIn;
-		this.easeOutPercent = ease.easeOut;
+		if(ease.easeIn !== undefined) this.easeInPercent = ease.easeIn;
+		if(ease.easeOut !== undefined) this.easeOutPercent = ease.easeOut;
 
 	}
 
-
 	this.finalFrame = function(action){
-		
+
 		if(action.onComplete){
 			action.onComplete();
 		}
-		
+
 		action.owner.doRemoveAction(action);
-		
+
 	};
 
 	this.setEase = function(){
@@ -62,7 +59,7 @@ var HHgAction = function (owner, totalDelta, startValue, totalTime, ease, onComp
 		}else{
 			this.easeOutTime = 0;
 		}
-		
+
 		this.middlePercent =  1 - (this.easeOutPercent + this.easeInPercent) ;
 		this.middlePercent = this.middlePercent + (this.easeInPercent * (1 - this.easeAreaUnderCurveMod));
 		this.middlePercent = this.middlePercent + (this.easeOutPercent * (1 - this.easeAreaUnderCurveMod));
@@ -73,9 +70,9 @@ var HHgAction = function (owner, totalDelta, startValue, totalTime, ease, onComp
 
 	this.whatShouldIDoThisFrame = function(deltaT){
 		this.timeSoFar += deltaT;
-		
+
 		if(this.timeSoFar >= this.totalTime){
-			
+
 			if(this.isSpecial === true){
 				this.updateFunc(this.lastValue.returnVectorSubtractedFromVector(this.endXY));
 			}else if(this.isXY === true){
@@ -83,33 +80,33 @@ var HHgAction = function (owner, totalDelta, startValue, totalTime, ease, onComp
 			}else{
 				this.updateFunc( this.totalDelta - this.lastValue );
 			}
-			
+
 			this.finalFrame(this);
 
 			return;
 		}
 
-		
+
 		if(this.timeSoFar < this.easeInTime){
-			
+
 			this.deltaPercent = this.easeInPercent * ( deltaT/this.easeInTime * ( this.timeSoFar/this.easeInTime ) );
-		
-			
+
+
 		}else if(this.timeSoFar > this.totalTime - this.easeOutTime ){
-			
+
 			this.deltaPercent = this.easeOutPercent * ( deltaT/this.easeOutTime * ( (this.totalTime - this.timeSoFar)/this.easeOutTime) );
-			
-			
+
+
 		}else{
 
 			this.deltaPercent = this.middlePercent * ( deltaT / this.middleTime );
 		}
 
 		this.lastPercent += this.deltaPercent;
-	
+
 		if(this.isSpecial === true){
 				this.tempXY = this.specialUpdateFunc(this.lastPercent, this);
-				
+
 				this.deltaValue = this.tempXY.returnSubtract(this.lastValue);
 				this.lastValue = this.tempXY;
 		}else if(this.isXY === true){
@@ -126,7 +123,7 @@ var HHgAction = function (owner, totalDelta, startValue, totalTime, ease, onComp
 
 	};
 
-	
+
 }
 
 HHg.HHgActionCommands = {
@@ -145,16 +142,15 @@ function HHgActionMoveBy(owner, totalDelta, startValue, totalTime, ease, onCompl
 
 	HHgAction.call(this, owner, totalDelta, startValue, totalTime, ease, onComplete);
 
-
 	this.updateFunc = this.owner.setPositionInScreenBy.bind(owner);
 
 	this.setEase();
-	
+
 }
 HHg.HHgActionCommands.makeChildOfAction(HHgActionMoveBy);
 
 function HHgActionMoveForever(owner, vectorPerSecond, ease){
-	HHgAction.call(this, owner, null, ease);
+	HHgAction.call(this, owner, null, null, null, ease, null);
 
 	this.perSecondXY = vectorPerSecond;
 
@@ -163,7 +159,7 @@ function HHgActionMoveForever(owner, vectorPerSecond, ease){
 		owner.setPositionInScreenBy(this.perSecondXY.returnVectorScaledBy(deltaT));
 		//could add ease in option here
 	}
-	
+
 }
 HHg.HHgActionCommands.makeChildOfAction(HHgActionMoveForever);
 
@@ -172,29 +168,30 @@ HHg.HHgActionCommands.makeChildOfAction(HHgActionMoveForever);
 function HHgActionRotateBy(owner, totalDelta, startValue, totalTime, ease, onComplete){
 	HHgAction.call(this, owner, totalDelta, startValue, totalTime, ease, onComplete);
 
-	
+
 	this.updateFunc = this.owner.setRotationOriginalBy.bind(owner);
 
 	this.setEase();
-	
-	
+
+
 }
 HHg.HHgActionCommands.makeChildOfAction(HHgActionRotateBy);
 
 
 function HHgActionRotateForever(owner, speed, ease){
-	HHgAction.call(this, owner, null, ease);
+	HHgAction.call(this, owner, null, null, null, ease, null);
 
 	this.perSecondN = speed;
+	var that = this;
 
 	this.whatShouldIDoThisFrame = function(deltaT){
-
-		owner.setRotationOriginalBy(this.perSecondN * deltaT);
+		console.log(that.perSecondN);
+		owner.setRotationOriginalBy(that.perSecondN * deltaT);
 		//could add ease in option here
 
 	}
-	
-	
+
+
 }
 HHg.HHgActionCommands.makeChildOfAction(HHgActionRotateForever);
 
@@ -209,14 +206,14 @@ function HHgActionScaleBy(owner, totalDelta, startValue, totalTime, ease, onComp
 	this.updateFunc = this.owner.setScaleOriginalBy.bind(owner);
 
 	this.setEase();
-	
-	
+
+
 }
 HHg.HHgActionCommands.makeChildOfAction(HHgActionScaleBy);
 
 
 function HHgActionScaleForever(owner, vectorPerSecond, ease){
-	HHgAction.call(this, owner, null, ease);
+	HHgAction.call(this, owner, null, null, null, ease, null);
 
 	this.perSecondXY = HHg1Vector.returnVectorSubtractedFromVector(vectorPerSecond);
 
@@ -226,8 +223,8 @@ function HHgActionScaleForever(owner, vectorPerSecond, ease){
 		//could add ease in option here
 
 	}
-	
-	
+
+
 }
 HHg.HHgActionCommands.makeChildOfAction(HHgActionScaleForever);
 
@@ -239,7 +236,7 @@ function HHgActionFollowQuad(owner, controlXY, endXY, startValue, totalTime, eas
 	this.endXY = endXY;
 	this.endX = endXY.getX();
 	this.endY = endXY.getY();
-	
+
 	this.startX = this.startValue.getX();
 	this.startY = this.startValue.getY();
 	this.controlXY = controlXY;
@@ -251,7 +248,7 @@ function HHgActionFollowQuad(owner, controlXY, endXY, startValue, totalTime, eas
 	this.isSpecial = true;
 
 	this.specialUpdateFunc = function(distance, action){
-		
+
 		return new HHgVector2(action.getXorYAlongQuad(distance, action.startX, action.controlX, action.endX),
 								action.getXorYAlongQuad(distance, action.startY, action.controlY, action.endY));
 	}
@@ -262,23 +259,23 @@ function HHgActionFollowQuad(owner, controlXY, endXY, startValue, totalTime, eas
 
 	var negT;
 	this.getXorYAlongQuad = function(t, p1, p2, p3) {
-		
+
     	negT = 1 - t;
     	return (negT * negT * p1 + 2 * negT * t * p2 + t * t * p3);
-    	
+
 	}
-	
+
 }
 HHg.HHgActionCommands.makeChildOfAction(HHgActionFollowQuad);
 
 
 function HHgActionTimer(owner, totalTime, onComplete){
 
-HHgAction.call(this, owner,null,null, totalTime, false, onComplete);
+HHgAction.call(this, owner, null, null, totalTime, ease, onComplete);
 
 	this.whatShouldIDoThisFrame = function(deltaT){
 		this.timeSoFar += deltaT;
-		
+
 		if(this.timeSoFar >= this.totalTime){
 			that.finalFrame(this);
 
@@ -287,8 +284,8 @@ HHgAction.call(this, owner,null,null, totalTime, false, onComplete);
 
 
 	}
-	
-	
+
+
 }
 HHg.HHgActionCommands.makeChildOfAction(HHgActionTimer);
 
--- HHgGame.js
+++ HHgGame.js
@@ -7,7 +7,7 @@ var HHgGame = {};
 	HHgGame.doStart = function(){
 		//GAME STARTS HERE
 	console.log("start game");
-		if(false){
+		if(true){
 
 			var theOne = HHgGetHolder({w:100,h:100});
 			theOne.doMoveToNewParent({parent: HHgGameHolder,position: new HHgVector2(-200,-200), isScreenPos: true});
@@ -17,17 +17,20 @@ var HHgGame = {};
 			theOne.setIsDraggable(true);
 
 
-			var listOfHolder = [];
-			listOfHolder.push(theOne);
+			var listOfHolders = [];
+			listOfHolders.push(theOne);
 			theOne.setPositionInScreenTo(new HHgVector2(0,450));
-			theOne.doActionMoveInScreenBy({x:0,y: -700,time: 10, easeIn: 20 });
-			theOne.doActionRotateBy({rotation:360,time: 30});
+
+			//theOne.doActionMoveInScreenBy({x:0,y: -700,time: 10, easeIn: 20 });
+
+			//theOne.doActionRotateBy({rotation:360,time: 30});
+
 			//theOne.doActionScaleTo({scaleX:0.25,scaleY:0.25,time: 30});
 
 
 			var randomSprite = function(holder){
 
-				var int1 = window.HHg.returnRandomIntLowIncHighExcl(0,3);
+				var int1 = HHg.returnRandomIntLowIncHighExcl(0,3);
 				var name = "orange";
 				if(int1 === 0){
 					name = "soccer";
@@ -46,33 +49,36 @@ var HHgGame = {};
 				var posy = HHg.returnRandomIntLowIncHighExcl(-500,500);
 				var testBall = HHgGetHolder({w:size,h:size});
 
-				testBall.doMoveToNewParent( {parent: listOfHolder[ HHg.returnRandomIntLowIncHighExcl(0, listOfHolder.length) ] , position: new HHgVector2(posx, posy) });
+				testBall.doMoveToNewParent( {parent: listOfHolders[ HHg.returnRandomIntLowIncHighExcl(0, listOfHolders.length) ] , position: new HHgVector2(posx, posy) });
 				randomSprite(testBall);
 				testBall.setMouseable(true);
 				testBall.setIsDraggable(true);
-				testBall.doActionRotateBy({rotation: HHg.returnRandomInt(120,720), time: HHg.returnRandomInt(5,35)});
 
-				listOfHolder.push(testBall)
+				//testBall.doActionRotateBy({rotation:360,time: 30});
+
+				testBall.doActionRotateForever(HHg.returnRandomInt(1,35));
+				//testBall.doAddParagraphText({text: "" + i, color: "#cccccc", vAlign: "center", hAlign: "center", fontSize: "60", shadow: {x: 3, y: 3, color: "#000000", blur: 2}});
+
+
+				listOfHolders.push(testBall)
 			}
 		}
 
-		var theOne;
-		if(true){
 
+		if(false){
+			var theOne;
 			theOne = HHgGetHolder({w:300,h:300});
 			theOne.doMoveToNewParent({parent: HHgGameHolder,position: new HHgVector2(-950,-540), isScreenPos: false});
-			
+
 			theOne.doAddSprite("pool");
 			theOne.test = "pool";
 			theOne.setMouseable(true);
 			theOne.setIsDraggable(true);
 			//theOne.doAddCanvasText({text: "TBLOCK", vAlign: "bottom", hAlign: "right", fontSize: "200"});
-			
+
 			theOne.doAddParagraphText({text: "TBLOCK\nStuff and stuff", color: "#cccccc", vAlign: "center", hAlign: "center", fontSize: "40", shadow: {x: 5, y: 5, color: "#000000", blur: 2}});
 
 
-
-			
 			//theOne.setBackgroundRGBA(new HHgColorRGBA(255,0,0));
 			var theTwo;
 			setTimeout(function(){
@@ -82,7 +88,7 @@ var HHgGame = {};
 				theTwo.test = "orange";
 				theTwo.setMouseable(true);
 				theTwo.setIsDraggable(true);
-				
+
 		}, 3000);
 
 		var theThree;
@@ -96,14 +102,14 @@ var HHgGame = {};
 					theThree.setMouseable(true);
 					theThree.setIsDraggable(true);
 				}
-			
+
 
 		}, 4000);
-			
-			
-			
+
+
+
 			//theOne.doActionMoveInScreenBy({x:1900,y: 0,time: 10, easeIn: 25, easeOut: 25});
-			
+
 			//theOne.doActionRotateBy({rotation:360,time: 10, easeIn: 25, easeOut: 25});
 
 			theOne.doActionFollowQuad({cx: 0, cy: 540, x: 960, y: -540, time: 10, easeIn: 25, easeOut: 25 });
